67fbd6ff61bddc57b8a253d77e37469ba67d77c4
Christian Fredericksen <christian.fredericksen@yahoo.com>
2019-10-28 00:15:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,14 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["I", "am", "really", "learning"]
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["I", "am", "really", "learning"]
+  array << "arrays!"
+  array.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
