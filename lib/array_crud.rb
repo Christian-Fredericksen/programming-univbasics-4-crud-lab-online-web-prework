c3db9e15156fa14d1e13b8110f8df6b1e647a7a9
Christian Fredericksen <christian.fredericksen@yahoo.com>
2019-10-28 00:32:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  
+  arrays_element
 end
 
 def remove_element_from_start_of_array(array)
